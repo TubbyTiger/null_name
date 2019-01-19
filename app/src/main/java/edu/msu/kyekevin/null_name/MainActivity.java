c2b8e769bd1617376c88fec50f9126108b932686
Jeremy <intern10@phinternprogram.onmicrosoft.com>
2019-01-19 08:37:11 -0500

    voice record

--- app/src/main/java/edu/msu/kyekevin/null_name/MainActivity.java
+++ app/src/main/java/edu/msu/kyekevin/null_name/MainActivity.java
@@ -1,18 +1,68 @@
 package edu.msu.kyekevin.null_name;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.app.Service;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int Record_Limit = 100;
+    private TextView mVoiceRecorded; // what system recorded
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mVoiceRecorded = (TextView) findViewById(R.id.voiceRecorded);
+        ImageButton mSpeakBtn = findViewById(R.id.btnRecord);
+        mSpeakBtn.setOnClickListener(new View.OnClickListener() {
 
-
+            @Override
+            public void onClick(View v) {
+                startVoiceRecord();
+            }
+        });
     }
 
+    private void startVoiceRecord() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "recording voice");
+        try {
+            startActivityForResult(intent, Record_Limit);
+        } catch (ActivityNotFoundException a) {
 
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case Record_Limit: {
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String TextRecorded = result.get(0); // command user input
+                    mVoiceRecorded.setText(TextRecorded);
+                }
+                break;
+            }
+
+        }
+    }
 }
